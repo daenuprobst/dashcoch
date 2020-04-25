f74591072d8d4f8b670221f4c560e0467a8a73e9
Daniel Probst <daenuprobst@gmail.com>
2020-04-25 11:15:38 +0200

    Removed gevent

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 # workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = "gevent"
+# worker_class = "gevent"
 
 # Keep things clean and neat (and leak-free)
 max_requests = 1200
