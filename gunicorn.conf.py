1620c999f244cf57c555e2dd68d849819748df58
Daniel Probst <daenuprobst@gmail.com>
2020-04-25 11:08:40 +0200

    Removed calculation of n of workers

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 import multiprocessing
 
-workers = multiprocessing.cpu_count() * 2 + 1
+# workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gevent"
 
 # Keep things clean and neat (and leak-free)
